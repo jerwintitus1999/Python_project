9caeb4e8ab9e273bbeceec30b7708e6bfa084da0
jerwintitus1999 <61309846+jerwintitus1999@users.noreply.github.com>
2024-09-30 11:05:25 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.title("Hello World with Balloons!")
+st.write("Click the button to celebrate.")
+
+if st.button('Celebrate!'):
+    st.balloons()
+
+# import streamlit as st
+
+st.title("User Input Example")
+
+name = st.text_input("Enter your name")
+if name:
+    st.write(f"Hello, {name}! Welcome to Streamlit!")
+
+# import streamlit as st
+import pandas as pd
+
+st.title("Map of Chennai")
+
+# Coordinates for Chennai (latitude, longitude)
+chennai_coordinates = pd.DataFrame({
+    'lat': [12.925947999046729],
+    'lon': [80.23581106680246]
+})
+
+# Display the map with Chennai's coordinates
+st.map(chennai_coordinates)
+
+checkBox = st.radio(
+    "What is your favourite food",
+    ["Pizza", "Burger", "Dosa"],
+    index= None,
+)
+
+st.write("You selected: ", checkBox)
